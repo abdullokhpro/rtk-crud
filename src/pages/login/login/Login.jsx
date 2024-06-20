d3e3f27845c12d39f96daf65f51c61769308b345
abdullokhpro <abdulloxrasulov441@gmail.com>
2024-06-20 17:15:27 +0500

    secondal commit

--- src/pages/login/login/Login.jsx
+++ src/pages/login/login/Login.jsx
@@ -109,6 +109,7 @@ const Login = () => {
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled={isLoading}
               >
                 Sign in
               </button>
